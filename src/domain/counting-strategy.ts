f67faafeabc471081ec4d26f0fc849eef3a51774
JoãoPedroFrare <frarejp@gmail.com>
2026-09-19 11:36:00 -0300

    feat: add tests for condominium counting

--- src/domain/counting-strategy.ts
+++ src/domain/counting-strategy.ts
@@ -141,7 +141,7 @@ export class ConclaveCountingStrategy implements ICountingStrategy<ConclaveVote>
 
         for (const vote of votes) {
             allVotes++;
-            if (this.currentCall > 4 && !this.twoMostVoted.includes(vote.value.candidateId)) {
+            if (this.currentCall > 2 && !this.twoMostVoted.includes(vote.value.candidateId)) {
                 //Tolerance of 4 votings before the restriction of the candidates
                 throw new Error("Voted canditate is not one of the two most voted of the last call");
 
