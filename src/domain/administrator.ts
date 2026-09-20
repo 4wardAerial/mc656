80199de44ae874f89103bd403a73cce17e9af3a9
Ana Luiza <paro.analuiza@gmail.com>
2026-09-19 21:50:44 -0300

    fix: fix formatting of Administrator

--- src/domain/administrator.ts
+++ src/domain/administrator.ts
@@ -15,7 +15,7 @@ export class Administrator {
 
     registerVoter(voter: Voter): void {
         if (this.registeredVoters.has(voter.id)) {
-            throw new Error(`Voter with id ${voter.id} is already registered.`);
+            throw new Error('Voter with id ${voter.id} is already registered.');
         }
         this.registeredVoters.set(voter.id, voter);
     }
